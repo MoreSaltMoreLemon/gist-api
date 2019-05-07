c5c61ab4d451f4a4c0a764f263f8c301b19ae25b
Ezra Schwepker <ezraschwepker@gmail.com>
2019-05-06 17:20:46 -0700

    login working. create user auto logs in

--- db/schema.rb
+++ db/schema.rb
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2019_04_29_204432) do
   end
 
   create_table "users", force: :cascade do |t|
-    # t.string "uuid", default: ""
+    t.string "uuid", default: ""
     t.string "username"
     t.string "password_digest"
     t.string "email"
